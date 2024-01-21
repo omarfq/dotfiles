0f3abd910849ac9c3d705efbb655b757f07266dd
Omar Flores <43190119+omarfq@users.noreply.github.com>
2024-01-21 12:27:54 -0400

    Switch to Brewfilw for Homebrew installs

--- README.md
+++ README.md
@@ -8,6 +8,4 @@ Repository to store all dotfiles
 - Mission Control Preferences
 - Finder Show Path Bar
 - Trackpad (Three Finger Drag and Tap to Click)
-- .zshrc
 - Git (config and SSH)
-- Homebrew Installation
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,9 +4,5 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
-
-brew install --cask --no-quarantine google-chrome
-brew install --cask --no-quarantine visual-studio-code
+brew bundle --verbose
 
